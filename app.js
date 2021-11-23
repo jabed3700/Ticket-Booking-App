62619d8b1da1c92149a2b891377a0c8c9e37097a
jabed37 <jabed.aleshasolutions@gmail.com>
2021-11-23 13:13:52 +0600

    add coupon code validation

--- app.js
+++ app.js
@@ -1,178 +1,203 @@
 const app = Vue.createApp({
-    data(){
+    data() {
         return {
+            appliedCoupon: null,
+            couponCode: "",
+            coupons: [{
+                    code: "100TAKAOFF",
+                    discount: 100
+                },
+                {
+                    code: "200TAKAOFF",
+                    discount: 200
+                }
+            ],
             seatStates: {
                 sold: {
-                  text: "Sold",
-                  color: "#ff0000"
+                    text: "Sold",
+                    color: "#ff0000"
                 },
                 available: {
-                  text: "Available",
-                  color: "#fff"
+                    text: "Available",
+                    color: "#fff"
                 },
                 booked: {
-                  text: "Booked",
-                  color: "gray"
+                    text: "Booked",
+                    color: "gray"
                 },
                 selected: {
-                  text: "Selected",
-                  color: "#00ff00"
+                    text: "Selected",
+                    color: "#00ff00"
                 }
-              },
-              seats: [
-                {
-                  name: "A1",
-                  type: "available",
-                  price: 500
+            },
+            seats: [{
+                    name: "A1",
+                    type: "available",
+                    price: 500
                 },
                 {
-                  name: "A2",
-                  type: "available",
-                  price: 500
+                    name: "A2",
+                    type: "available",
+                    price: 500
                 },
                 {
-                  name: "A3",
-                  type: "available",
-                  price: 500
+                    name: "A3",
+                    type: "available",
+                    price: 500
                 },
                 {
-                  name: "A4",
-                  type: "available",
-                  price: 500
+                    name: "A4",
+                    type: "available",
+                    price: 500
                 },
                 {
-                  name: "B1",
-                  type: "available",
-                  price: 450
+                    name: "B1",
+                    type: "available",
+                    price: 450
                 },
                 {
-                  name: "B2",
-                  type: "available",
-                  price: 450
+                    name: "B2",
+                    type: "available",
+                    price: 450
                 },
                 {
-                  name: "B3",
-                  type: "available",
-                  price: 450
+                    name: "B3",
+                    type: "available",
+                    price: 450
                 },
                 {
-                  name: "B4",
-                  type: "available",
-                  price: 450
+                    name: "B4",
+                    type: "available",
+                    price: 450
                 },
                 {
-                  name: "C1",
-                  type: "sold",
-                  price: 500
+                    name: "C1",
+                    type: "sold",
+                    price: 500
                 },
                 {
-                  name: "C2",
-                  type: "sold",
-                  price: 500
+                    name: "C2",
+                    type: "sold",
+                    price: 500
                 },
                 {
-                  name: "C3",
-                  type: "sold",
-                  price: 500
+                    name: "C3",
+                    type: "sold",
+                    price: 500
                 },
                 {
-                  name: "C4",
-                  type: "sold",
-                  price: 500
+                    name: "C4",
+                    type: "sold",
+                    price: 500
                 },
                 {
-                  name: "D1",
-                  type: "available",
-                  price: 400
+                    name: "D1",
+                    type: "available",
+                    price: 400
                 },
                 {
-                  name: "D2",
-                  type: "available",
-                  price: 400
+                    name: "D2",
+                    type: "available",
+                    price: 400
                 },
                 {
-                  name: "D3",
-                  type: "available",
-                  price: 400
+                    name: "D3",
+                    type: "available",
+                    price: 400
                 },
                 {
-                  name: "D4",
-                  type: "available",
-                  price: 400
+                    name: "D4",
+                    type: "available",
+                    price: 400
                 },
                 {
-                  name: "E1",
-                  type: "available",
-                  price: 300
+                    name: "E1",
+                    type: "available",
+                    price: 300
                 },
                 {
-                  name: "E2",
-                  type: "available",
-                  price: 300
+                    name: "E2",
+                    type: "available",
+                    price: 300
                 },
                 {
-                  name: "E3",
-                  type: "booked",
-                  price: 300
+                    name: "E3",
+                    type: "booked",
+                    price: 300
                 },
                 {
-                  name: "E4",
-                  type: "booked",
-                  price: 300
+                    name: "E4",
+                    type: "booked",
+                    price: 300
                 },
                 {
-                  name: "F1",
-                  type: "available",
-                  price: 300
+                    name: "F1",
+                    type: "available",
+                    price: 300
                 },
                 {
-                  name: "F2",
-                  type: "available",
-                  price: 300
+                    name: "F2",
+                    type: "available",
+                    price: 300
                 },
                 {
-                  name: "F3",
-                  type: "available",
-                  price: 300
+                    name: "F3",
+                    type: "available",
+                    price: 300
                 },
                 {
-                  name: "F4",
-                  type: "available",
-                  price: 300
+                    name: "F4",
+                    type: "available",
+                    price: 300
                 }
-              ]
+            ]
         }
     },
-    computed:{
-        selectedSeats(){
-            let seatCount = this.seats.filter((value) => value.type ==="selected");
+    computed: {
+        selectedSeats() {
+            let seatCount = this.seats.filter((value) => value.type === "selected");
             return seatCount;
         },
         totalCost() {
             let tc = 0;
             this.selectedSeats.forEach((seat) => {
-              tc += seat.price;
+                tc += seat.price;
             });
-          
+
             return tc;
-          }
+        }
     },
     methods: {
-        seatChoose(i){
+        seatChoose(i) {
             let seatSelect = this.seats[i];
-            if(seatSelect.type==="sold" || seatSelect.type==="booked"){
+            if (seatSelect.type === "sold" || seatSelect.type === "booked") {
                 alert('You can not select this seat');
                 return;
             };
-            if(this.selectedSeats.length > 2){
+           
+            seatSelect.type = seatSelect.type == "available" ? 'selected' : 'available';
+            
+            if (this.selectedSeats.length > 2) {
                 alert('You can not select more than 3 seats');
                 return;
             };
-            seatSelect.type = seatSelect.type=="available" ? 'selected':'available';
         }
     },
-    watch:{
-
+    watch: {
+        couponCode(newValue) {
+            if (newValue.length === 10) {
+              let searchedCoupons = this.coupons.filter(
+                (item) => item.code === newValue
+              );
+          
+              if (searchedCoupons.length === 1) {
+                this.appliedCoupon = searchedCoupons[0];
+                this.couponCode = "";
+              } else {
+                alert("Coupon not valid!");
+              }
+            }
+          }
     }
 })
 app.mount('#app')
